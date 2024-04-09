08fcd85be615710504344e29bff8e08414d38e7b
Nima Parifard <paryfardnim@gmail.com>
2024-04-09 18:42:14 +0330

    add model part b to git

--- code/P5/model_resolution.py
+++ code/P5/model_resolution.py
@@ -2,15 +2,24 @@
 import torch
 from torch import nn, optim
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
 class MLP(nn.Module):
-    def __init__(self):
+
+    def __init__(self, activation_fn=nn.ReLU):
         super(MLP, self).__init__()
         self.layers = nn.Sequential(
             nn.Linear(27, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 3)
+            activation_fn(),
+            nn.Linear(64, 128),
+            activation_fn(),
+            nn.Linear(128, 256),
+            activation_fn(),
+            nn.Linear(256, 128),
+            activation_fn(),
+            nn.Linear(128, 64),
+            activation_fn(),
+            nn.Linear(64, 3)
         )
 
     def forward(self, x):
